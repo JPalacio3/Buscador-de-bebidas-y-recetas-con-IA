ae1625858de852bcc839ba214c7a6f05678441f9
JPalacio3 <jpalaciocano3@gmail.com>
2025-07-09 20:07:49 -0600

    feat: agregar carga diferida para la página de inicio utilizando Suspense y Spinner

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,27 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from "./views/IndexPage";
+
 import Layout from "./layouts/Layout";
 import Spinner from "../public/spiner/Spinner";
 
 const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
+const IndexPage = lazy(() => import("./views/IndexPage"));
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} index></Route>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <IndexPage />
+              </Suspense>
+            }
+            index
+          ></Route>
+
           <Route
             path="/favoritos"
             element={
