cebc43a5f9aa184358d271b05d20f1e9349aadfe
JPalacio3 <jpalaciocano3@gmail.com>
2025-07-18 16:19:14 -0600

    corregir la llamada a streamText para que sea await en la generación de recetas

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -14,7 +14,7 @@ export default {
 
     while (attempt < maxRetries) {
       try {
-        const result = await streamText({
+        const result = streamText({
           // model: openrouter("meta-llama/llama-3.3-70b-instruct:free"),
           // model: openrouter('google/gemini-2.5-pro-exp-03-25:free'),
           // model: openrouter("google/gemma-3-4b-it:free"),
