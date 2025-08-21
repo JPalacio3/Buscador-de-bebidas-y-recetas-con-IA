00ca377efa62282b8a5a92043482f22d39c48a45
JPalacio3 <jpalaciocano3@gmail.com>
2025-08-20 20:00:55 -0600

    Actualizar la firma de la función generateRecipe para permitir un retorno nulo y mejorar el manejo de errores en la generación de recetas.

--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -1,19 +1,41 @@
 import type { StateCreator } from "zustand";
 import AIService from "../services/AIService";
+import type { NotificationSliceType } from "./notificationSlice";
 
 export type AISlice = {
   recipe: string;
   isGenerating: boolean;
-  generateRecipe: (prompt: string) => Promise<string>;
+  generateRecipe: (prompt: string) => Promise<string | null>;
 };
 
-export const createAISlice: StateCreator<AISlice, [], [], AISlice> = (set) => ({
+export const createAISlice: StateCreator<
+  AISlice & NotificationSliceType,
+  [],
+  [],
+  AISlice
+> = (set, get) => ({
   recipe: "",
   isGenerating: false,
   generateRecipe: async (prompt) => {
     set({ recipe: "", isGenerating: true });
-    const data = await AIService.generateRecipe(prompt);
-    set({ recipe: data, isGenerating: false });
-    return data;
+    try {
+      const data = await AIService.generateRecipe(prompt);
+      set({ recipe: data, isGenerating: false });
+      return data;
+    } catch (error: unknown) {
+      let errorMessage = "No se recibió respuesta de la IA.";
+      if (error instanceof Error) {
+        if (error.message.includes("429")) {
+          errorMessage =
+            "Has alcanzado el límite de solicitudes. Por favor, espera un momento antes de volver a intentarlo.";
+        }
+      }
+      get().showNotification({
+        text: errorMessage,
+        error: true,
+      });
+      set({ isGenerating: false });
+      return null;
+    }
   },
 });
